07d8a78fbd4acfb14ca16d87036e763db378557e
hasan-gurbuz <95375955+hasan-gurbuz@users.noreply.github.com>
2022-04-13 02:27:11 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height/1.034,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage("https://www.upload.ee/image/14046565/Group_27.png"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Tarla", style: TextStyle(fontSize: 28, color: Colors.white, fontWeight: FontWeight.w600),),
+                  Text("Tarla'dan evimize, organik ve taze!", style: TextStyle(fontSize: 13, color: Colors.white, fontWeight: FontWeight.w500),),
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 18),
+                        child: Container(
+                          height: 40,
+                            width: 308,
+                            decoration: BoxDecoration(
+                              color: Colors.lightBlue,
+                              borderRadius: BorderRadius.circular(6),
+                            ),
+                            child: Center(child: Text("Üreticileri Listele", style: TextStyle(color: Colors.white, fontSize: 15),))),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 44),
+                    child: Row(
+                      children: [
+                        Text("Üye Girişi"),
+                        Text("Hesap Oluştur"),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
